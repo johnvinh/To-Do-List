b3170db8312b6e35d6069681765d674562c4a68d
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-01-04 22:03:46 -0500

    tasks can now be removed

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function addTask(e)
 {
-    const task = document.querySelector('#new-task').value;
-    document.querySelector('#items').innerHTML += `<li>${task}</li>`;
+    const task = document.createElement('span');
+    task.innerHTML = document.querySelector('#new-task').value;
+
+    const listItem = document.createElement('li');
+    listItem.appendChild(task);
+    const removeButton = document.createElement('input');
+    removeButton.setAttribute('type', 'button');
+    removeButton.setAttribute('value', 'Remove');
+    removeButton.addEventListener('click', (e) => listItem.parentElement.removeChild(listItem));
+    listItem.appendChild(removeButton);
+    document.querySelector('#items').appendChild(listItem);
 }
 
 document.querySelector('#add-task').addEventListener('click', addTask);
