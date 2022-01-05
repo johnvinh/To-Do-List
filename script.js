a7e05820939e7099356373cc15c4f9b0dbcf6746
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-01-04 22:22:21 -0500

    changed remove button styling slightly

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function addTask(e)
     const removeButton = document.createElement('input');
     removeButton.setAttribute('type', 'button');
     removeButton.setAttribute('value', 'Remove');
+    removeButton.setAttribute('class', 'remove');
     removeButton.addEventListener('click', (e) => listItem.parentElement.removeChild(listItem));
     listItem.appendChild(removeButton);
     document.querySelector('#items').appendChild(listItem);
