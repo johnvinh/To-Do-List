03ca58aa96401ca8619e78174205700c1abd27dd
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-01-05 20:18:16 -0500

    added a 'done' button for each item which strikes through the text

--- script.js
+++ script.js
@@ -3,14 +3,29 @@ function addTask(e)
     const task = document.createElement('span');
     task.innerHTML = document.querySelector('#new-task').value;
 
+    // Task content
     const listItem = document.createElement('li');
     listItem.appendChild(task);
+
+    // Remove Button
     const removeButton = document.createElement('input');
     removeButton.setAttribute('type', 'button');
     removeButton.setAttribute('value', 'Remove');
-    removeButton.setAttribute('class', 'remove');
+    removeButton.setAttribute('class', 'listbutton');
     removeButton.addEventListener('click', (e) => listItem.parentElement.removeChild(listItem));
     listItem.appendChild(removeButton);
+
+    // Mark as completed button
+    const doneButton = document.createElement('input');
+    doneButton.setAttribute('type', 'button');
+    doneButton.setAttribute('value', 'Done');
+    doneButton.setAttribute('class', 'listbutton');
+    doneButton.addEventListener('click', (e) =>
+    {
+        listItem.classList.add('completed');
+    });
+    listItem.appendChild(doneButton);
+
     document.querySelector('#items').appendChild(listItem);
     // clear the textbox after the task is added for convenience
     document.querySelector('#new-task').value = "";
